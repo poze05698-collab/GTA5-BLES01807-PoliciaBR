405ba79a7d87d1102074aff107e2bba66ef2780e
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:01:37 -0300

    Create wanted.cpp

---
+++ police/wanted.cpp
@@ -0,0 +1,14 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void WantedSystem()
+    {
+        // Futuramente:
+        // - Verificar o nível de procurado do suspeito
+        // - Identificar crimes cometidos
+        // - Definir prioridade da ocorrência
+        // - Compartilhar informação com as unidades policiais
+        // - Atualizar o status após prisão ou fuga
+    }
+}
