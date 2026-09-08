d7eba9b027cb209b9a6b5e6249d0b1cfd99bf7bd
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:07:30 -0300

    Add RequestBackup function in backup.cpp
    Implement initial structure for backup request functionality.

---
+++ police/backup.cpp
@@ -0,0 +1,14 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void RequestBackup()
+    {
+        // Futuramente:
+        // - Solicitar unidade de apoio
+        // - Definir tipo de apoio
+        // - Enviar viaturas para a ocorrência
+        // - Acompanhar a chegada do backup
+        // - Cancelar o pedido de apoio
+    }
+}
