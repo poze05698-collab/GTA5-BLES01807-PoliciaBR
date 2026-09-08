c16924310303da8048ee63361f6d9e01ee1c573b
poze05698-collab <poze05698@gmail.com>
2026-09-08 10:54:09 -0300

    Add ApproachNPC function for NPC interactions

---
+++ police/approach.cpp
@@ -0,0 +1,14 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void ApproachNPC()
+    {
+        // Futuramente:
+        // - Identificar o NPC selecionado
+        // - Iniciar abordagem
+        // - Fazer NPC parar
+        // - Iniciar diálogo
+        // - Verificar comportamento do suspeito
+    }
+}
