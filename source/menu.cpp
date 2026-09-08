3f89d8421c12cdf433d3031b8404e605a8b4b3ec
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:21:11 -0300

    Implement menu functions for police operations
    Added several menu functions for police operations.

--- source/menu.cpp
+++ source/menu.cpp
@@ -12,18 +12,57 @@ namespace Menu
     void Show()
     {
         aberto = true;
+        MainMenu();
     }
 
     void Update()
     {
         if (!aberto)
         {
-            Show();
+            return;
         }
+
+        // A navegação pelo controle será implementada
+        // depois que definirmos a plataforma PS3.
     }
 
     void Close()
     {
         aberto = false;
+    }
+
+    void MainMenu()
+    {
+        // Menu principal da Polícia BR
+    }
+
+    void PoliceMenu()
+    {
+        // Serviço policial
+    }
+
+    void OperationsMenu()
+    {
+        // Operações policiais
+    }
+
+    void VehicleMenu()
+    {
+        // Viaturas
+    }
+
+    void EquipmentMenu()
+    {
+        // Equipamentos
+    }
+
+    void CallsMenu()
+    {
+        // Chamados policiais
+    }
+
+    void RankMenu()
+    {
+        // Patente e carreira
     }
 }
