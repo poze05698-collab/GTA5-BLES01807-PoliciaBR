d9b1fb3df3172322f04b7662a964d1a2e5031251
poze05698-collab <poze05698@gmail.com>
2026-09-08 10:49:01 -0300

    Update service.cpp

--- police/service.cpp
+++ police/service.cpp
@@ -1,1 +1,21 @@
+#include "../include/police.h"
 
+namespace Police
+{
+    static bool emServico = false;
+
+    void Init()
+    {
+        emServico = false;
+    }
+
+    void EnterService()
+    {
+        emServico = true;
+    }
+
+    void ExitService()
+    {
+        emServico = false;
+    }
+}
