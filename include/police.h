8d28f1188ee951dc6fc5db1236cf69ae2a74a9a5
poze05698-collab <poze05698@gmail.com>
2026-09-08 05:00:50 -0300

    Create police.h

---
+++ include/police.h
@@ -0,0 +1,35 @@
+#ifndef POLICE_H
+#define POLICE_H
+
+namespace Police
+{
+    void Init();
+
+    void EnterService();
+    void ExitService();
+
+    void SpawnVehicle();
+    void Equipment();
+
+    void ToggleSiren();
+
+    void ApproachNPC();
+    void StartPursuit();
+    void ArrestSuspect();
+    void FineSuspect();
+
+    void WantedSystem();
+
+    void PoliceCalls();
+    void RequestBackup();
+
+    void PoliceOperations();
+    void FavelaOperations();
+
+    void Radio();
+    void Uniform();
+
+    void RankSystem();
+}
+
+#endif
