bdbe6cb723e94cffe3b05d6236d8d66e8c868e83
poze05698-collab <poze05698@gmail.com>
2026-09-08 10:51:06 -0300

    Create vehicles.cpp

---
+++ police/vehicles.cpp
@@ -0,0 +1,14 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void SpawnVehicle()
+    {
+        // A implementação real será conectada ao GTA V PS3 posteriormente.
+    }
+
+    void ToggleSiren()
+    {
+        // A implementação real será conectada ao GTA V PS3 posteriormente.
+    }
+}
