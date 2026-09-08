04d26b89a7c260eccb55c9684044c60e9c92411b
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:16:12 -0300

    Add PoliceStations function with future plans

---
+++ police/stations.cpp
@@ -0,0 +1,20 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void PoliceStations()
+    {
+        // Futuramente:
+        // - Selecionar delegacia
+        // - Polícia Militar
+        // - Polícia Civil
+        // - Polícia Federal
+        // - PRF
+        // - Polícia Rodoviária Estadual
+        // - Polícia Científica
+        // - Entrar na delegacia
+        // - Registrar ocorrência
+        // - Consultar ocorrências
+        // - Encerrar atendimento
+    }
+}
