eba253ddef1baccba399b2808fe38062e9d39368
poze05698-collab <poze05698@gmail.com>
2026-09-08 10:46:50 -0300

    Implement basic input functions in input.cpp

---
+++ source/input.cpp
@@ -0,0 +1,34 @@
+#include "../include/input.h"
+
+namespace Input
+{
+    bool Up()
+    {
+        return false;
+    }
+
+    bool Down()
+    {
+        return false;
+    }
+
+    bool Left()
+    {
+        return false;
+    }
+
+    bool Right()
+    {
+        return false;
+    }
+
+    bool Select()
+    {
+        return false;
+    }
+
+    bool Back()
+    {
+        return false;
+    }
+}
