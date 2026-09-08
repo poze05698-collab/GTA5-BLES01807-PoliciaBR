1b7391bb1f8b567538e64718fad6fe16b96cea77
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:12:39 -0300

    Add FavelaOperations function in favela.cpp

---
+++ police/favela.cpp
@@ -0,0 +1,17 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void FavelaOperations()
+    {
+        // Futuramente:
+        // - Selecionar área de operação
+        // - Criar ocorrência
+        // - Criar suspeitos
+        // - Solicitar backup
+        // - Iniciar operação
+        // - Controlar o andamento
+        // - Finalizar a operação
+        // - Registrar resultado e pontuação
+    }
+}
