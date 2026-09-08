df19ea18a947fd05668e5043dba7fff2423a5afe
poze05698-collab <poze05698@gmail.com>
2026-09-08 10:59:16 -0300

    Create fine.cpp

---
+++ police/fine.cpp
@@ -0,0 +1,14 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void FineSuspect()
+    {
+        // Futuramente:
+        // - Identificar o cidadão abordado
+        // - Selecionar a infração
+        // - Definir o valor da multa
+        // - Registrar a ocorrência
+        // - Adicionar pontuação ao sistema policial
+    }
+}
