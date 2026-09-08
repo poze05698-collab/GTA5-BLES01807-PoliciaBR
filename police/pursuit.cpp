5b05502f374dde43715dab922e63e5d1f9c47232
poze05698-collab <poze05698@gmail.com>
2026-09-08 10:54:48 -0300

    Implement StartPursuit function in pursuit.cpp

---
+++ police/pursuit.cpp
@@ -0,0 +1,15 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void StartPursuit()
+    {
+        // Futuramente:
+        // - Identificar o veículo/suspeito
+        // - Iniciar perseguição
+        // - Ativar alerta policial
+        // - Solicitar apoio
+        // - Acompanhar o suspeito
+        // - Encerrar a perseguição
+    }
+}
