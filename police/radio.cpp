2cd2df9828d457e0450f42d3dd169222c950e4c0
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:05:28 -0300

    Create Radio function in police/radio.cpp
    Implement initial structure for Police Radio functionality.

---
+++ police/radio.cpp
@@ -0,0 +1,15 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void Radio()
+    {
+        // Futuramente:
+        // - Comunicação com a central
+        // - Mensagens de rádio
+        // - Código da ocorrência
+        // - Solicitação de unidades
+        // - Comunicação durante perseguições
+        // - Comunicação durante operações
+    }
+}
