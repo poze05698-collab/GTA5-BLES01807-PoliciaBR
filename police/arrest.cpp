a3f95d855941620e70179b21ad07cdad31a66ca5
poze05698-collab <poze05698@gmail.com>
2026-09-08 10:56:12 -0300

    Add ArrestSuspect function in arrest.cpp
    Implement ArrestSuspect function with future steps outlined.

---
+++ police/arrest.cpp
@@ -0,0 +1,14 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void ArrestSuspect()
+    {
+        // Futuramente:
+        // - Identificar o suspeito abordado
+        // - Verificar se pode ser preso
+        // - Aplicar a prisão
+        // - Encerrar a ocorrência
+        // - Adicionar pontuação ao policial
+    }
+}
