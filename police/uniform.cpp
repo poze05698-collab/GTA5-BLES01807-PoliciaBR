f5938a29a7f7b1ff21efb472be3c22161d11f86f
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:39:07 -0300

    Create uniform.cpp

---
+++ police/uniform.cpp
@@ -0,0 +1,19 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void Uniform()
+    {
+        // Futuramente:
+        // - Polícia Militar
+        // - Polícia Civil
+        // - Polícia Federal
+        // - Polícia Rodoviária Federal
+        // - Polícia Rodoviária Estadual
+        // - Polícia Científica
+        // - Uniforme operacional
+        // - Uniforme tático
+        // - Uniforme de serviço
+        // - Equipamentos visuais da farda
+    }
+}
