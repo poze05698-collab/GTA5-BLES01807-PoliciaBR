75c958f69ab287a14562a6cda7e45bc770feb7d3
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:03:36 -0300

    Add PoliceCalls function with future plans

---
+++ police/calls.cpp
@@ -0,0 +1,16 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void PoliceCalls()
+    {
+        // Futuramente:
+        // - Receber chamados da central
+        // - Criar diferentes tipos de ocorrência
+        // - Informar localização
+        // - Definir prioridade
+        // - Aceitar ou recusar o chamado
+        // - Encaminhar o jogador para a ocorrência
+        // - Registrar a conclusão
+    }
+}
