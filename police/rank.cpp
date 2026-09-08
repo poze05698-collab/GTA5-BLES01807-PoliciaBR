97cbd98bb938389b799a5b8d971062b3bc7a1633
poze05698-collab <poze05698@gmail.com>
2026-09-08 11:17:53 -0300

    Implement initial RankSystem function

---
+++ police/rank.cpp
@@ -0,0 +1,22 @@
+#include "../include/police.h"
+
+namespace Police
+{
+    void RankSystem()
+    {
+        // Futuramente:
+        // - Recruta
+        // - Soldado
+        // - Cabo
+        // - Sargento
+        // - Tenente
+        // - Capitão
+        // - Major
+        // - Coronel
+        // - Sistema de experiência
+        // - Pontuação por ocorrência
+        // - Promoção de patente
+        // - Perda de pontuação
+        // - Histórico da carreira
+    }
+}
